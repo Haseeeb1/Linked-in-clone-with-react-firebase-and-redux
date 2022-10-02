5c32372e8b788ed3e74e6da0ce530b73eab1f77f
Haseeb Ul Hassan <100109683+Haseeeb1@users.noreply.github.com>
2022-10-02 10:40:45 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAArT0vNP5BX8abXqjXm3mUGLvnGYIYoBc",
-  authDomain: "linkedin-clone-8d8be.firebaseapp.com",
-  projectId: "linkedin-clone-8d8be",
-  storageBucket: "linkedin-clone-8d8be.appspot.com",
-  messagingSenderId: "681601510016",
-  appId: "1:681601510016:web:3189a75eb0f139c7fdd465",
-  measurementId: "G-4WGVZ7Q5EB",
+  apiKey: //your api key,
+  authDomain: //your authdomain,
+  projectId: //your project id,
+  storageBucket: //...,
+  messagingSenderId: //....,
+  appId: //....,
+  measurementId: //....,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
